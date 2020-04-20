9a0913b3f4cd1d09be259313d04a73c18555634f
Francisco Rascon <thednasoftware@gmail.com>
2020-04-20 16:36:43 -0700

    Initial commit

---
+++ TrackExpenses/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrackExpenses.MvcApplication" Language="C#" %>
---
+++ TrackExpenses/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrackExpenses.Startup))]
+namespace TrackExpenses
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
